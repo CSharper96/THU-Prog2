4583e737b36751f78960bc71c05ce23d82a58b45
Thomas <thomashorny@gmx.net>
2020-06-24 15:33:02 +0200

    Exercise11 finished

---
+++ src/HornyMerk/prog2/exercises/set11/currencyconverter/CurrencyConverterApp.java
@@ -0,0 +1,27 @@
+package HornyMerk.prog2.exercises.set11.currencyconverter;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class CurrencyConverterApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("CurrencyConverter.fxml"));
+
+        primaryStage.setTitle("Currency Converter");
+        primaryStage.setScene(new Scene(root, 300, 265));
+        primaryStage.setResizable(false);
+
+        primaryStage.show();
+    }
+}
